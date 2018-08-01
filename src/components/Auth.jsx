c03ca79a5084e5263b78be1226b273e880a62b01
Aldo Choque <achoque1400@gmail.com>
2018-07-31 21:59:28 -0500

    Componente estatico Private Route

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -9,6 +9,7 @@ import {
 } from 'react-router-dom'
 
 const fakeAuth={
+    // permite manejar si estoy autenticado o no
     isAuthenticated:false,
     authenticate(cb){
         this.isAuthenticated=true,
@@ -23,6 +24,28 @@ const fakeAuth={
 const Home=()=><h3>Home</h3>
 const Public=()=><h3>Contenido Publico</h3>
 const Protected=()=><h3>Contenido Protegido</h3>
+
+const AuthButton=withRouter(({history})=>(
+    (fakeAuth.isAuthenticated) 
+    ?  
+    <div>
+    <h2>!Bienvenido!:)</h2>
+    <button onClick={()=>fakeAuth.signout(()=>history.push('/'))}>
+    Salir
+    </button>
+    </div>
+    :
+    <h2>!No estas logeado :( !</h2>
+))
+
+const PrivateRoute=({component:Component,rest})=>(
+    <Route {...rest} render={(props)=>(
+        fakeAuth.isAuthenticated
+        ? <Component {...props} />
+        : <Redirect to={{pathname:'/login',state:{from:props.location}}}/>
+    )} 
+    />
+)
 
 class Login extends Component{
     constructor(...props){
@@ -53,15 +76,16 @@ const AuthSite=()=>(
         <div>
             <AuthButton />
             <ul>
-                <li><Link to="/">Home</Link>Home</li>
-                <li><Link to="/public">Home</Link></li>
-                <li><Link to="/protected">Home</Link></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/public">Pagina publica</Link></li>
+                <li><Link to="/protected">Pagina protegida</Link></li>
             </ul>
-            <switch>
+            <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/public" exact component={Public} />               
                 <PrivateRoute path="/protected" component={Protected}/>
-            </switch>
+                <Route path="/login" component={Login}/>
+            </Switch>
         </div>
     </Router>
 )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/';
 import StaticSite from './components/Router'
+import AuthSite from './components/Auth'
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<StaticSite />, document.getElementById('root'));
+// ReactDOM.render(<StaticSite />, document.getElementById('root'));
+ReactDOM.render(<AuthSite />, document.getElementById('root'));
